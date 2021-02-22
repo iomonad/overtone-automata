7ee369f390fa7411408cfb6261068749da867cd3
Clement Van Truyart <me@trosa.io>
2021-02-22 11:18:10 +0100

    Added deps + code cleanup

--- src/overtone_automata/core.clj
+++ src/overtone_automata/core.clj
@@ -1,6 +1,1 @@
 (ns overtone-automata.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- test/overtone_automata/core_test.clj
+++ test/overtone_automata/core_test.clj
@@ -1,7 +1,3 @@
 (ns overtone-automata.core-test
   (:require [clojure.test :refer :all]
             [overtone-automata.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
