0742484850d3baf07c44eac3634f6552cf7a37ec
Clement Van Truyart <me@trosa.io>
2021-02-22 11:00:20 +0100

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to overtone-automata
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/overtone_automata/core_test.clj
@@ -0,0 +1,7 @@
+(ns overtone-automata.core-test
+  (:require [clojure.test :refer :all]
+            [overtone-automata.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
